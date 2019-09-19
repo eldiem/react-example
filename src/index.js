8d0bdd810904f55c67f2587222553bb7aba5aa5c
eldiem <dev.dmlee@gmail.com>
2019-09-19 16:42:55 +0900

    react-lifecycle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App message="Hello Message" />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor: 클래스의 시작");
+    this.state = {
+      helloLifeCycle: "helloLifeCycle"
+    };
+  }
+  componentDidMount() {
+    console.log("componentDidMount: 첫 렌더링이 완료됨");
+    console.log("-------------업데이트 시작-------------");
+    console.log("setState 진행");
+    this.setState({
+      helloLifeCycle: "updated"
+    });
+  }
+
+  componentDidUpdate(prevPrpps, prevState) {
+    console.log("componentDidUpdate: 컴포넌트가 업데이트 됨");
+    // 업데이트 된 후 의 현재 state
+    console.log(this.state, " 업데이트 후");
+    // 업데이트 되기 전 state
+    console.log(prevState, " 업데이트 전");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    console.log("창 닫을 때 동작해서 확인 불가");
+  }
+
+  render() {
+    console.log("render: 나는 렌더링을 합니다.");
+    return (
+      <div className="App">
+        <h1>Hello react LifeCycle</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);

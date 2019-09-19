372d6efa0c082b7a8e7db0d51ca227866a2de80b
eldiem <dev.dmlee@gmail.com>
2019-09-19 16:16:56 +0900

    props-example

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    // hello: 'hello app js!'
     count: 0
   };
 
   handleChange = () => {
-    this.setState({
-      hello: 'bye app js!'
-    });
-  };
-
-  conutUp = () => {
     this.setState({
       count: this.state.count + 1
     });
@@ -22,12 +15,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <div>{this.state.hello}</div> */}
-        {/* <button onClick={this.handleChange}>Click Me!</button> */}
-        <div>{this.state.count}</div>
-        <button onClick={this.conutUp}>Count up!</button>
+        <h3>index Props</h3>
+        <div className="props">
+          <span>{this.props.message}</span>
+        </div>
+
+        <h3>State</h3>
+        <div className="state">
+          {this.state.count}
+          <button onClick={this.handleChange}>Click up!</button>
+        </div>
+
+        <h3>App Props</h3>
+        <div className="inside-app-props">
+          <InsideApp
+            count={this.state.count}
+            handleChange={this.handleChange}
+          />
+        </div>
       </div>
     );
+  }
+}
+
+class InsideApp extends Component {
+  // App.js에서 InsideApp 컴포넌트를 받아오고, 
+  // 그 때 App 컴포넌트의 state와 메소드인 count 와 handleChange를 상속
+  render() {
+    return (
+      <div>
+        {this.props.count}
+        <button onClick={this.props.handleChange}>Click Me!</button>
+      </div>
+    )
   }
 }
 
